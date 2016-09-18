cdfdfbe813c058e06190a24cf9d50a26b8a4acf9
Coding Jerk <codingjerk@gmail.com>
2016-09-18 10:57:35 +0300

    Adding minimal spam interval option to spammer extension

--- extensions/spammer.py
+++ extensions/spammer.py
@@ -8,11 +8,25 @@ import time
 class Extension(base.Extension):
 	def __init__(self, config):
 		super().__init__(config)
+		self.last_spam = time.now()
 
 	def work(self):
-		if random.random() < self.config['chance']:
-			phrase = random.choice(self.config['phrases'])
-			self.reply(phrase)
-
 		time.sleep(1)
+
+		if random.random() >= self.config['chance']: return True
+		if not self.recently_spammed(): return True
+		self.spam()
+
 		return True
+
+	def spam(self):
+		phrase = random.choice(self.config['phrases'])
+		self.reply(phrase)
+
+		self.last_spam = time.time()
+
+	def recently_spammed(self):
+		time_from_last_spam = time.time() - self.last_spam
+		minimal_time = self.config['minimal-spam-interval']
+
+		return time_from_last_spam <= minimal_time
